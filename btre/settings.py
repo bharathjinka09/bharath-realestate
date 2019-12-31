1e4f84b589a4735402f2f8250b8a64ec6128c42e
bharathmieone <bharath@mieone.com>
2019-12-31 10:41:41 +0530

    Changed urls

--- btre/settings.py
+++ btre/settings.py
@@ -134,12 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 django_heroku.settings(locals())
